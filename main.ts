8d0cd06a085efef1ac23663ef3ef08f292c55478
bonaventuredivaga <119411798+bonaventuredivaga@users.noreply.github.com>
2022-12-06 12:21:52 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    a += 1
+	
 })
 input.onButtonPressed(Button.AB, function () {
-    a = 0
+	
 })
 input.onButtonPressed(Button.B, function () {
-    a += -1
+	
 })
 let a = 0
-a = 0
 basic.forever(function () {
-    if (a == 3) {
-        basic.showIcon(IconNames.Heart)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
+	
 })
