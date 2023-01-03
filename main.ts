951158e561c85042bcf2e883e0f9d5092855d4fa
bonaventuredivaga <119411798+bonaventuredivaga@users.noreply.github.com>
2023-01-03 12:39:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,26 @@
 input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
     number = randint(0, 2)
-    basic.showNumber(number)
     if (number == 0) {
         basic.showLeds(`
             . . . . .
@@ -11,25 +31,26 @@ input.onGesture(Gesture.Shake, function () {
             `)
     } else if (number == 1) {
         basic.showLeds(`
-            # # # # #
-            # # # # .
-            # # # # #
+            . . # # #
             . # # # #
             # # # # #
+            # # # # .
+            # # # . .
             `)
     } else if (number == 2) {
         basic.showLeds(`
-            . . . # .
-            . . # . #
             . # . # .
-            # # # . .
-            # # . . .
+            . # . # .
+            . # . # .
+            . . # . .
+            . # . # .
             `)
     }
+    basic.pause(2000)
+    basic.clearScreen()
 })
 let number = 0
 number = 0
-basic.showNumber(number)
 basic.forever(function () {
 	
 })
